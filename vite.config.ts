546a85795004e1d11041a82d973864f9f367e685
nathan927 <109018668+nathan927@users.noreply.github.com>
2024-11-02 17:59:22 +0800

    Update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/ThisDictation/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 }); 
